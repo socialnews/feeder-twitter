348622421c8a9a0bf5d2ce562e9b5fab977afaec
alexrevell <revellalex@gmail.com>
2015-06-13 14:24:12 +1200

    removed secret comments

--- twitterstream.rb
+++ twitterstream.rb
@@ -10,13 +10,6 @@ client = Twitter::Streaming::Client.new do |config|
   config.access_token_secret    = ENV["ACCESS_TOKEN_SECRET"]
 end
 
-# client = Twitter::Streaming::Client.new do |config|
-#   config.consumer_key       = "OkDa508rn7hucG4gBEsZtWWWB"
-#   config.consumer_secret    = "hvkhNWPoiJiXFcWrP0vELsvTvuDhT6Ywf8kwCGypUE3Exe7wwE"
-#   config.access_token    = "3243672536-oTCVDsPktpKcRYq2FNRvOHF0MrsQ9SgLHioXs1w"
-#   config.access_token_secret    = "ql9H6NoL62t9PpniZ7WQ82PibyGMZcPKL03KZ1AB0X7ws"
-# end
-
 topics = ["news"]
 client.filter(track: topics.join(",")) do |object|
   puts object.text if object.is_a?(Twitter::Tweet)
